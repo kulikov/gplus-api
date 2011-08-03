802a59be6e74955abe2d8e9a70a17620b960a19d
kulikov <kulikov.dm@gmail.com>
2011-08-03 17:45:33 +0400

    clean

--- Gplus/Profile.php
+++ Gplus/Profile.php
@@ -7,13 +7,7 @@ require_once 'Comment.php';
 
 class Profile
 {
-    const
-        COMMENTS_URL = 'https://plus.google.com/_/stream/getactivity/?updateId=%s',
-        POSTS_URL    = 'https://plus.google.com/%s/posts';
-
-    private
-        $_cacher    = null,
-        $_profileId = null;
+    private $_profileId = null;
 
     public function __construct($profileId)
     {
